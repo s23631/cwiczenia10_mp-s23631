824849a807c882ce1c81c353010a638cfeb7c93d
SoKamil <37556481+SoKamil@users.noreply.github.com>
2022-06-27 14:28:26 +0200

    Init

---
+++ cwiczenia10_mp-s23631/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using cwiczenia10_mp_s23631.Models;
+
+namespace cwiczenia10_mp_s23631.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+    }
+}
---
+++ cwiczenia10_mp-s23631/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace cwiczenia10_mp_s23631.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
